704fe1cc986ab1d15850b7b8b6f77aaefb66b993
Joao Barroso <joao.barroso@alu.ufc.br>
2024-03-24 13:28:03 -0300

    Aula 2

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,70 @@
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/poll/:pollId/vote", async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+    const { pollId } = voteOnPollParams.parse(request.params);
+
+    let { sessionId } = request.cookies;
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            // pesquisa direto pelo index
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        // o voto anterior é apagado e um novo é criado
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        });
+      } else if (userPreviousVoteOnPoll) {
+        return reply
+          .status(400)
+          .send({ message: "User already voted on this poll." });
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7, // 1 semana
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    return reply.status(201).send({ sessionId });
+  });
+}
